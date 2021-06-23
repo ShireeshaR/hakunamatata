f97efd26b01ec65a994fc3094c9f9d865427012d
ShireeshaR <65624715+ShireeshaR@users.noreply.github.com>
2021-06-23 14:44:14 +0530

    Update scrpt.js

--- scrpt.js
+++ scrpt.js
@@ -1,7 +1,7 @@
 
 <!-- Gainsight PX Tag-->
-  (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
-      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+  (function(n,t,a,e){var i="aptrinsic";n[i]=n[i]||function(){
+      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
   })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-H5BILVZODAED-2");
