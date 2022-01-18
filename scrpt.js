5db929eb6c6b2832ffe114389e5495870aedef3c
ShireeshaR <65624715+ShireeshaR@users.noreply.github.com>
2022-01-18 07:15:12 +0530

    Update scrpt.js

--- scrpt.js
+++ scrpt.js
@@ -37,14 +37,25 @@ function allowlogin(usermail){
         
          b = a.substr(8, 3);
          alert("Logged in user id :" + b);
-           aptrinsic.identify({
-                  id: 'unique-id',
-                  email: 'gibb@example.com',
-                  name: 'Monica',
-                  lastSeenDate: 1630419623719,
-                  role: "System",
-                  LastSeen: 1630419623719
-                    }),
+           //passing user and account objects:
+aptrinsic("identify",
+  {
+  //User Fields
+    "id": "unique-user-id", // Required for logged in app users
+    "email": "userEmail@address.com",
+    "firstName": "John",
+    "lastName": "Smith",
+    "signUpDate": 1522697426479, //unix time in ms
+    "plan" : "gold", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
+    "price" : 95.5,
+    "userHash": "" // optional transient for HMAC identification
+  },
+  {
+  //Account Fields
+    "id":"IBM", //Required
+    "name":"International Business Machine",
+    "Program": "Platinum" // flat custom attributes
+ });
           
                      window.location = "https://shireeshar.github.io/hakunamatata/index.html";
 
